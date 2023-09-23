5989106ae98813ae25982d5d36647484e7ed63be
Dave Hudson <dhudson@wbmi.com>
2023-09-23 15:18:14 -0500

    styling and code clean up

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -4,6 +4,7 @@ import { useStore } from "../../../app/stores/store";
 import { observer } from "mobx-react-lite";
 import { useEffect } from "react";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
+import ActivityFilters from "./ActivityFilters";
 
 // eslint-disable-next-line react-refresh/only-export-components
 export default observer(function ActivityDashboard() {
@@ -17,13 +18,13 @@ export default observer(function ActivityDashboard() {
   if (activityStore.loadingInitial)
     return <LoadingComponent content="Loading app" />;
 
-  return ( 
+  return (
     <Grid>
       <Grid.Column width="10">
         <ActivityList />
       </Grid.Column>
       <Grid.Column width="6">
-        <h2>Activity filters</h2>
+        <ActivityFilters />
       </Grid.Column>
     </Grid>
   );
